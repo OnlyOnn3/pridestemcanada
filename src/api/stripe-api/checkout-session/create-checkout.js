e436763808273cec75b0edf02cade5237cc61e5d
mrajput <mn334860@dal.ca>
2025-10-24 05:05:29 -0300

    Created Checkout session creater

---
+++ src/api/stripe-api/checkout-session/create-checkout.js
@@ -0,0 +1,35 @@
+import Stripe from 'stripe';
+
+
+const StripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2025-09-30 ',
+});
+
+export default async function handler(req, res)
+ {
+  if (req.method !== 'POST') {
+    const session = await StripeInstance.checkout.sessions.create({
+      payment_method_types: ['card'], //add other payment methods as needed
+      line_items: [
+        {
+          price_data: {
+            currency: 'cad', 
+            product_data: {
+              name: 'Dummy ticket',
+            },
+            unit_amount: 2000, //amount in cents
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+    //   success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,//Implement when session ids and db are setup
+      success_url: `${req.headers.origin}/stripe-pages/success`,
+      cancel_url: `${req.headers.origin}/stripe-pages/failiure`,
+    });
+    res.status(200).json({sessionId: session.id});
+    } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
