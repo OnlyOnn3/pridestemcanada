bafc21f7eb816a6125e6b4e4981a571d60837531
aduba <adubaangel7@gmail.com>
2025-11-21 18:25:26 -0400

    Added contect to the conference about us page

--- pride-stem-app/src/app/conference_about/page.jsx
+++ pride-stem-app/src/app/conference_about/page.jsx
@@ -25,13 +25,16 @@ export default function ConferenceAboutPage() {
           <h1 className={styles.heroTitle}>About the Conference</h1>
           <p className={styles.heroSubtitle}>
            Header caption
+           
           </p>
         </div>
       </motion.section>
 
       <motion.section className={styles.formSectionWrapper} {...fadeInUp}>
         <div className={styles.presentSection}>
-        <p>Content here</p>
+        <p>Join us from January *insert date* to *insert date* , 2026, at Dalhousie, where we will expect to welcome over 250 delegates from across Canada. This conference will spotlight the latest research, hear inspiring success stories, and highlight emerging leaders within the 2SLGBTQ+ STEM community. 
+
+        The diversification of STEM fields is essential if we are to mobilize talent in Canada, drive innovation and promote discovery across all scientific fields. The conference brings together leading experts, innovators, and changemakers to explore actionable strategies for dismantling barriers to 2SLGBTQ+ success in STEM. In addition to great science, we will also hear about increasing visibility, improving recruitment and retention of queer and trans scientists, and strategies to foster truly inclusive cultures in academia, industry, and beyond.</p>
         </div>
       </motion.section>
     </div>
