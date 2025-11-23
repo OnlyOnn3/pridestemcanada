23d6ae0c39d6e9335cd9504d945568d49cdf6d01
mrajput <mn334860@dal.ca>
2025-11-23 13:37:05 -0400

    fixed vercel issue

--- pride-stem-app/src/app/register/components/FormSection.jsx
+++ pride-stem-app/src/app/register/components/FormSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import RegistrationForm from "../RegistrationForm";
+import RegistrationForm from "../registrationForm";
 import styles from "../../page.module.css";
 
 export default function FormSection() {
